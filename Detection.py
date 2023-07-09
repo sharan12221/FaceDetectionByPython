4c7eed550ca15740d170019a6778c53bdfdbb711
Sharanwakade <117678416+sharan12221@users.noreply.github.com>
2023-07-09 07:12:45 -0400

    Create Detection.py

---
+++ Detection.py
@@ -0,0 +1,35 @@
+import cv2
+
+# Load the pre-trained face detection model
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Open the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read webcam frame
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale for face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Perform face detection
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Draw rectangles around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Face Detection', frame)
+
+    # Check for key press event
+    key = cv2.waitKey(1)
+
+    # Close the window if 'q' key is pressed
+    if key == ord('q'):
+        break
+
+# Release the webcam and close the windows
+cap.release()
+cv2.destroyAllWindows()
